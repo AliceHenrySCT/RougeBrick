669cf0c24a73e2e0102e3991ed055a8d6812e696
AliceHenrySCT <AliceHenry431@gmail.com>
2025-07-04 18:00:10 -0700

    Remove black empty space at bottom

--- components/Game.tsx
+++ components/Game.tsx
@@ -268,7 +268,17 @@ const Game: React.FC<GameProps> = ({ onQuit }) => {
 };
 
 const styles = StyleSheet.create({
-  container: { flex: 1, backgroundColor: 'black' },
+  container: { 
+    flex: 1, 
+    backgroundColor: 'black',
+    position: 'absolute',
+    top: 0,
+    left: 0,
+    right: 0,
+    bottom: 0,
+    width: '100%',
+    height: '100%'
+  },
   quitButton: {
     position: 'absolute',
     top: 40,
